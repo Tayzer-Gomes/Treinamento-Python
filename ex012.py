493c2851e0c208a6c148f652bcb203f04b58069e
Tayzer Gomes <tayzergomes@gmail.com>
2022-07-01 22:35:03 -0300

    Cálculo de preço com desconto

--- ex012.py
+++ ex012.py
@@ -1,4 +1,4 @@
-#Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto!
+#Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
 p = float(input('Digite o preço do produto: '))
 np = p - (5/100)
