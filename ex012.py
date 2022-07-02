4fe1203e30ca74baaad8acd1a163f7fe60fcdf37
Tayzer Gomes <tayzergomes@gmail.com>
2022-07-01 22:31:53 -0300

    Calco de preço com desconto

---
+++ ex012.py
@@ -0,0 +1,5 @@
+#Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto!
+
+p = float(input('Digite o preço do produto: '))
+np = p - (5/100)
+print('O novo valor com desconto é: {} reais!'.format(np))
