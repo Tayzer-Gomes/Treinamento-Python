fb500a8b47150addda8318f6c6e493bfbfe6c0c3
Tayzer Gomes <tayzergomes@gmail.com>
2022-07-03 21:14:51 -0300

    cálculo de aluguel de veículo

---
+++ ex015.py
@@ -0,0 +1,10 @@
+#Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais foi alugado.
+# Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por KM rodado.
+
+
+Km = float(input('Informe a quilometragem percorrida: '))
+dias = int(input('Informe quantos dias de aluguel: '))
+V_dia = dias * 60
+rodado = Km * 0.15
+preco = V_dia + rodado
+print('O preço total a pagar é {}'.format(preco))
