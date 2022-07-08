41f97739ce60b8d362c10640c252916d05036b78
Tayzer Gomes <tayzergomes@gmail.com>
2022-07-07 22:39:32 -0300

    Selecionando parte inteira de um número real

---
+++ ex016.py
@@ -0,0 +1,16 @@
+#Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
+
+#from math import trunc
+#import math
+#num = float(input('Digite um número: '))
+#print('O número digitado foi {} e sua parte inteira é:'.format(num), math.trunc(num))
+
+
+
+import math
+num = float(input('Digite um número: '))
+print('O número digitado foi {} e sua parte inteira é:'.format(num), math.ceil(num))
+
+
+
+
