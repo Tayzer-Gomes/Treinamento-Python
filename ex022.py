7c785edb4003b00c785ad9b830f2f5d59768278f
Tayzer Gomes <tayzergomes@gmail.com>
2022-07-15 21:03:08 -0300

    Funções para manipulações de string

---
+++ ex022.py
@@ -0,0 +1,13 @@
+#Crie um programa que leia o nome completo de uma pessoa e mostre:
+# O nome com todas as letras maiúsculas e minúsculas.
+# Quantas letras ao todo (sem considerar espaços).
+# Quantas letras tem o primeiro nome.
+
+nome = str(input('Digite seu nome: ')).strip()
+print('Seu nome com letras maiúsculas é {}'.format(nome.upper()))
+print('Seu nome com letras maiúsculas é {}'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
+
+
+
+
