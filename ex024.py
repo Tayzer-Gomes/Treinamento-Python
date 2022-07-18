3032187018909604a308daef5482ed34fe212c9e
Tayzer Gomes <tayzergomes@gmail.com>
2022-07-18 18:58:38 -0300

    Manipulação de strings

--- ex024.py
+++ ex024.py
@@ -1,4 +1,4 @@
-# Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome “SANTO”.
+# Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome “SANTO”!
 
 cidade = str(input('Em que cidade você mora? ')).strip()
 print(cidade[0:].upper() == 'SANTO')
