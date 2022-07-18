6336f6531e47e4e417e711c5498d54912d71b18f
Tayzer Gomes <tayzergomes@gmail.com>
2022-07-18 19:00:09 -0300

    Manipulando strings

--- ex025.py
+++ ex025.py
@@ -1,4 +1,4 @@
-#Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.
+#Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome!
 
 nome = str(input('Qual seu nome? '))
 print('Seu nome tem silva? {}'.format('SILVA' in nome.upper()))
